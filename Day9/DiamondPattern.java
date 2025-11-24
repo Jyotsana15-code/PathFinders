5d290d577e16142b84b1877d48159a34af5dae3b
Jyotsana15-code <jyotsanapriya2006@gmail.com>
2025-11-24 21:18:38 +0530

    file diamond pattern updated

--- Day9/DiamondPattern.java
+++ Day9/DiamondPattern.java
@@ -5,29 +5,17 @@ import java.util.Scanner;
 public class DiamondPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter an odd number (max width of diamond): ");
+        System.out.print("Enter max width of diamond (odd number): ");
         int n = sc.nextInt();
-        if (n % 2 == 0) {
-            n = n - 1;
-            System.out.println("Even input converted to odd: " + n);
-        }
-        int mid = (n / 2) + 1;
-        for (int i = 1; i <= mid; i++) {
-            int stars = 2 * i - 1;
-            int spaces = (n - stars) / 2;
-            for (int s = 0; s < spaces; s++) System.out.print(" ");
-            // print stars
-            for (int st = 0; st < stars; st++) System.out.print("*");
-            System.out.println();
-        }
-        for (int i = mid - 1; i >= 1; i--) {
-            int stars = 2 * i - 1;
-            int spaces = (n - stars) / 2;
-            for (int s = 0; s < spaces; s++) System.out.print(" ");
-            for (int st = 0; st < stars; st++) System.out.print("*");
-            System.out.println();
+        if (n % 2 == 0) n--;
+        int mid = n / 2;
+        for (int i = 0; i < n; i++) {
+            int spaces = Math.abs(mid - i);
+            int stars = n - 2 * spaces;
+            System.out.println(" ".repeat(spaces) + "*".repeat(stars));
         }
         sc.close();
     }
 }
+
 
